a211455dff74c33b055a996f5cbcc8dfed16bc57
scott-22 <scotthao65@gmail.com>
2026-08-16 11:00:45 -0400

    Add quotes

--- templates/data/featured.lisp
+++ templates/data/featured.lisp
@@ -11,4 +11,33 @@
     
     (("\"Whereof one cannot speak, thereof one must be silent.\"" :scramble)
      ("Wittgenstein, *Tractatus*" :fade))
+    
+    (("\"The simulacrum is never that which conceals the truth—" :scramble)
+     ("it is the truth which conceals that there is none." :fade)
+     ("The simulacrum is true.\"" :fade)
+     ("Baudrillard, *Simulacra and Simulation*" :fade))
+    
+    (("\"To parody a well-known saying," :scramble)
+     ("I shall say that a little formalism turns one away from History," :fade)
+     ("but that a lot brings one back to it." :fade)
+     ("Barthes, *Mythologies*" :fade))
+    
+    (("\"How can it be that every child succeeds" :scramble)
+     ("in acquiring such a rich system so early in life," :fade)
+     ("in an apparently unintentional manner, without the need for explicit instruction?\"" :fade)
+     ("Chomsky, *On Nature and Language*" :fade))
+    
+    (("\"When it started out, it was an awful lot of fun.\"" :scramble)
+     ("Perlis, *SICP*" :fade))
+    
+    (("\"I’m doing badly, I’m doing well, whichever you prefer.\"" :scramble)
+     ("Kafka, *Letters to Milena*" :fade))
+    
+    (("\"Nothing could be more comfortable than" :scramble)
+     ("writing about the ballet from books..." :fade)
+     ("It was like being in love with someone he had never seen.\"" :fade)
+     ("Kawabata, *Snow Country*" :fade))
+    
+    (("\"We must know, we will know!\"" :scramble)
+     ("Hilbert, *September 1930 Address*" :fade))
 )
